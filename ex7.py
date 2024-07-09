f78663e0f0eea756a9cc9436be14717e26b60504
rami <rami.toma99@gmail.com>
2024-07-09 21:25:58 +0200

    more :D

---
+++ ex7.py
@@ -0,0 +1,7 @@
+one, two, three = input("Enter three things seperated by a space: ").split()
+
+print("\n")
+
+print("the things u wrote are: ", one, two, three)
+
+#DONE
---
+++ ex8.py
@@ -0,0 +1,5 @@
+text = "I have {totalMoney} dollars so I can buy {quantity} football for {price:.2f} dollars."
+
+print(text.format(totalMoney = 1000, quantity = 3, price = 450))
+
+#DONE
